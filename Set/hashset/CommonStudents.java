fee3efafdf81bff2046c8ffc9f89b5626077a75e
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-09 15:34:39 -0500

    Add files via upload

---
+++ Set/hashset/CommonStudents.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class CommonStudents {
+    public static void main(String[] args) {
+        HashSet<String> CourseA = new HashSet<>(Arrays.asList("Alice","bob","Vinny"));
+        HashSet<String> CourseB =new HashSet<>(Arrays.asList("Charlie","bob","donna"));
+
+        HashSet<String> common=new HashSet<>(CourseA);
+        common.removeAll(CourseB);  //intersection of two sets, keeps only common elements
+
+        System.out.println("Students enrolled in both courses "+ common);
+    }
+}
