2699eef10d4cac529a93f2ef5239ef520cf31425
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-09 15:05:37 -0500

    Add files via upload

---
+++ Set/hashset/DuplicateWordsFinder.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class DuplicateWordsFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a paragraph");
+        String paragraph = sc.nextLine().toLowerCase();
+
+        String[] words=paragraph.split("\\W+ ");
+        HashSet<String> seen=new HashSet<>();
+        HashSet<String> duplicates=new HashSet<>();
+
+        for(String word:words){
+            if(!seen.add(word)){
+                duplicates.add(word);
+            }
+        }
+        System.out.println("Duplicate words:"+duplicates);
+        sc.close();
+
+    }
+}
